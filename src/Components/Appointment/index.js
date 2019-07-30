20d48fee685c30c6ef9bf1d9ce7981e82a9138e8
Vitor Hugo Scobin <vhscobin@gmail.com>
2019-07-30 00:59:32 -0300

    Add vaidations in Appointments

--- src/Components/Appointment/index.js
+++ src/Components/Appointment/index.js
@@ -1,30 +1,56 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { parseISO, formatRelative } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import PropTypes from 'prop-types';
 
 import { Container, Left, Avatar, Info, Name, Time } from './styles';
 
-export default function Appointment({ data }) {
+export default function Appointment({ data, onCancel }) {
+  const dateParsed = useMemo(() => {
+    return formatRelative(parseISO(data.date), new Date(), {
+      locale: pt,
+      addSuffix: true,
+    });
+  }, [data.date]);
   return (
-    <Container>
+    <Container past={data.past}>
       <Left>
         <Avatar
           source={{
             uri: data.provider.avatar
-              ? data.provider.avatar.url
+              ? data.provider.avatar.url.replace('localhost', '192.168.0.104')
               : `https://api.adorable.io/avatars/120/${data.provider.name}.png`,
           }}
         />
         <Info>
           <Name>{data.provider.name}</Name>
-          <Time>Em 3 horas</Time>
+          <Time>{dateParsed}</Time>
         </Info>
       </Left>
 
-      <TouchableOpacity onPress={() => {}}>
-        <Icon name="event-busy" size={20} color="#f64c75" />
-      </TouchableOpacity>
+      {data.cancelable && !data.canceled_at && (
+        <TouchableOpacity onPress={onCancel}>
+          <Icon name="event-busy" size={20} color="#f64c75" />
+        </TouchableOpacity>
+      )}
     </Container>
   );
 }
+
+Appointment.propTypes = {
+  onCancel: PropTypes.func.isRequired,
+  data: PropTypes.shape({
+    date: PropTypes.string.isRequired,
+    past: PropTypes.bool.isRequired,
+    cancelable: PropTypes.bool.isRequired,
+    canceled_at: PropTypes.string,
+    provider: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      avatar: PropTypes.shape({
+        url: PropTypes.string,
+      }),
+    }).isRequired,
+  }).isRequired,
+};
