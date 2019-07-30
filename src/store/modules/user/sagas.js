01fa3439e2b00a1f688c42dbca0a339fd5df7655
Vitor Hugo Scobin <vhscobin@gmail.com>
2019-07-30 00:22:44 -0300

    Config Redux and Saga

---
+++ src/store/modules/user/sagas.js
@@ -0,0 +1,27 @@
+import { all, takeLatest, call, put } from 'redux-saga/effects';
+import { Alert } from 'react-native';
+import api from '~/services/api';
+
+import { updateProfileSuccess, updateProfileFailure } from './actions';
+
+export function* updataProfile({ payload }) {
+  try {
+    const { name, email, avatar_id, ...rest } = payload.data;
+
+    const profile = Object.assign(
+      { name, email, avatar_id },
+      rest.oldPassword ? rest : {}
+    );
+
+    const response = yield call(api.put, 'users', profile);
+
+    Alert.alert('Sucesso', 'Perfil atualizado com sucesso');
+
+    yield put(updateProfileSuccess(response.data));
+  } catch (err) {
+    Alert.alert('Erro ao atualizar', 'Verifique seus dados');
+    yield put(updateProfileFailure());
+  }
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updataProfile)]);
