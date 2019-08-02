42db41e5a8178a7881b0688cc9902b29441f332b
Vitor Hugo Scobin <vhscobin@gmail.com>
2019-08-02 12:31:33 -0300

    Updating Confirm page

--- src/pages/New/Confirm/index.js
+++ src/pages/New/Confirm/index.js
@@ -1,8 +1,64 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useMemo } from 'react';
+import { TouchableOpacity } from 'react-native';
 
-// import { Container } from './styles';
+import { formatRelative, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 
-export default function Confirm() {
-  return <View />;
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import Background from '~/Components/Background';
+import { Container, Avatar, Name, Time, SubmitButton } from './styles';
+import api from '~/services/api';
+
+export default function Confirm({ navigation }) {
+  console.tron.log(navigation);
+
+  const provider = navigation.getParam('provider');
+  const time = navigation.getParam('time');
+
+  const timeFormatted = useMemo(
+    () => formatRelative(parseISO(time), new Date(), { locale: pt }),
+    [time]
+  );
+
+  async function handleAddAppointment() {
+    await api.post('appointments', {
+      provider_id: provider.id,
+      date: time,
+    });
+
+    navigation.navigate('Dashboard');
+  }
+
+  return (
+    <Background>
+      <Container>
+        <Avatar
+          source={{
+            uri: provider.avatar
+              ? provider.avatar.url.replace('localhost', '192.168.0.104')
+              : `https://api.adorable.io/avatars/120/${provider.name}.png`,
+          }}
+        />
+        <Name>{provider.name}</Name>
+        <Time>{timeFormatted}</Time>
+
+        <SubmitButton onPress={() => handleAddAppointment()}>
+          Confirmar agendamento
+        </SubmitButton>
+      </Container>
+    </Background>
+  );
 }
+
+Confirm.navigationOptions = ({ navigation }) => ({
+  title: 'Confirmar agendamento',
+  headerLeft: () => (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.goBack();
+      }}
+    >
+      <Icon name="chevron-left" size={20} color="#fff" />
+    </TouchableOpacity>
+  ),
+});
